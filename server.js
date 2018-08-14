2070c1b84b71a4dfdb96d4fd7928c1a984c53ca5
Garcia2001SO <garciapdev@gmail.com>
2018-08-14 17:15:16 -0500

    added stars collecting and scores. tutorial finished

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 var players = {}
+var star = {
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50
+};
+var scores = {
+    blue: 0,
+    red: 0
+};
 
 app.use(express.static(__dirname + '/public'));
 
@@ -22,8 +30,14 @@ io.on('connection', function(socket){
         team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
     };
 
-    //send the players object to the new player
+    //sends the players object to the new player
     socket.emit('currentPlayers', players);
+
+    //sends the star object to the new player
+    socket.emit('starLocation', star);
+
+    //sends the current scores to the new player
+    socket.emit('scoreUpdate', scores);
 
     //update all other players of the new player
     socket.broadcast.emit('newPlayer', players[socket.id]);
@@ -44,6 +58,19 @@ io.on('connection', function(socket){
 
         //emits a message to all players about the player that moved
         socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+
+    socket.on('starCollected', function(){
+        if(players[socket.id].team === 'red'){
+            scores.red += 10;
+        }else{
+            scores.blue += 10;
+        }
+
+        star.x = Math.floor(Math.random() * 700) + 50;
+        star.y = Math.floor(Math.random() * 500) + 50;
+        io.emit('starLocation', star);
+        io.emit('scoreUpdate', scores);
     });
 });
 
